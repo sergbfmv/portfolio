8ed5325ed1cfbf534c1c218b0c0fb41e2cb78279
Сергей <sergbfmv@mail.ru>
2023-08-10 17:13:23 +0300

    Added social links

--- src/components/socialLinks/SocialLinks.tsx
+++ src/components/socialLinks/SocialLinks.tsx
@@ -4,9 +4,18 @@ import {S} from './SocialLinks_Styles'
 
 
 const socialItemsData = [
-    {iconId: 'instagram'},
-    {iconId: 'tg'},
-    {iconId: 'linkedin'},
+    {
+        iconId: 'instagram',
+        href: 'https://www.instagram.com/serg_vale'
+    },
+    {
+        iconId: 'tg',
+        href: 'https://t.me/serg_vale'
+    },
+    {
+        iconId: 'linkedin',
+        href: 'https://www.linkedin.com/in/%D1%81%D0%B5%D1%80%D0%B3%D0%B5%D0%B9-%D0%B8%D0%B3%D0%BD%D0%B0%D1%82%D0%BE%D0%B2-1656b1207/'
+    },
 ]
 export const SocialLinks: React.FC = () => {
     return (
@@ -15,7 +24,7 @@ export const SocialLinks: React.FC = () => {
                 {socialItemsData.map((s, index) => {
                     return (
                         <S.SocialItem key={index}>
-                            <S.SocialLink href="">
+                            <S.SocialLink href={s.href} target='_blank'>
                                 <Icon iconId={s.iconId} width={'30px'} height={'30px'} viewBox={'0 0 30 30'}/>
                             </S.SocialLink>
                         </S.SocialItem>
