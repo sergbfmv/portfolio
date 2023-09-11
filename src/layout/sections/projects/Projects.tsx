f46b1fb52a91ab21e1c57b99ee628cde76b08709
Сергей <sergbfmv@mail.ru>
2023-09-11 21:48:34 +0300

    Added project counter

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -5,10 +5,10 @@ import {TabMenu, TabStatusType} from "./tabMenu/TabMenu";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Project} from "./project/Project";
 import proj1 from '../../../assets/images/proj1.webp'
-import proj2 from '../../../assets/images/proj2.webp'
 import proj3 from '../../../assets/images/proj3.webp'
 import proj4 from '../../../assets/images/proj4.webp'
 import projBatman from '../../../assets/images/proj-Batman.webp'
+import projCount from '../../../assets/images/projCount.webp'
 import {Container} from "../../../components/Container";
 import {S} from './Projects_Styles'
 import {AnimatePresence, motion} from "framer-motion"
@@ -42,12 +42,15 @@ const projectsData = [
         id: 1
     },
     {
-        title: 'Timer',
-        text: 'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',
-        techText: 'HTML , JavaScript, SASS, React',
-        src: proj2,
+        title: 'Counter',
+        text: 'This is sample project counter. You can add min and max value and save it in local storage for saving you count.',
+        techText: 'HTML, CSS, JavaScript, React, React-router-dom',
+        src: projCount,
         type: 'react',
-        id: 2
+        projDemoLink: 'https://sergbfmv.github.io/Batman/index.html',
+        projCodeLink: 'https://github.com/sergbfmv/Counter',
+        id: 2,
+
     },
     {
         title: 'ToDo list',
